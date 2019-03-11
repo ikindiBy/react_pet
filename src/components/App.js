8f8228a12aa63f8b3479e3770aa9f6edae86a1b5
ikindiBy <ikindi@mail.ru>
2019-03-11 09:00:54 +0300

    task_1: try dif methods of creating elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent, Fragment } from "react";
 import "../styles/App.css";
 
+const createE = React.createElement;
+
+function Button(props) {
+  return (
+    <button onClick={props.handleClick}>{props.name.toUpperCase()}</button>
+  );
+}
+
+function MainText(props) {
+  return createE("h1", { className: "mainText" }, props.content);
+}
+
+const Form = createE(
+  "form",
+  { className: "searchFrom" },
+  createE("label", { className: "label" }, "Type name of film or actor"),
+  createE("input", { type: "text", value: "And here is a child" }),
+  <Button name="Search" />
+);
+
+class FooterContent extends PureComponent {
+  render() {
+    return <h3>React.Minsk.2019</h3>;
+  }
+}
+
 class App extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <div>
-        <h1>My boilerplate for React! </h1>
-        <div>
-          <p>Lorem ipsum ....</p>
-        </div>
-      </div>
+      <Fragment>
+        <header>{Form}</header>
+        <main>
+          {createE(MainText, { content: "No films, but wait..." }, null)}
+        </main>
+        <footer>
+          <FooterContent />
+        </footer>
+      </Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import "./styles/style.css";
 
 import App from "./components/App.js";
 
-console.log("ww-w-w-w-w-w-w-w-w-w-w-w-w-w");
-
 const root = document.getElementById("root");
 
 ReactDOM.render(<App />, root);
