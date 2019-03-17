d52e4214a66a03d50834d7848726d99b409db372
ikindiBy <ikindi@mail.ru>
2019-03-18 01:30:49 +0300

    react-pet: add redux, jest, NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,17 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js"
-    // path: path.resolve(__dirname, "./dist"),
-    // filename: "index_bundle.js"
-    // publicPath: "/dist"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node-modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { sourceMap: true }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-              config: {
-                path: "src/postcss.config.js"
-              }
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: { sourceMap: true }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
+module.exports = (env, options) => {
+  let isProdMode = options.mode === "production";
+
+  console.log("isProdMode=", isProdMode, options.mode);
+
+  let plugins = [
+    new CaseSensitivePathsPlugin(),
     new HtmlWebpackPlugin({
       hash: false,
       template: "./src/index.html",
@@ -55,12 +20,76 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css"
       // chunkFilename: "[id].css"
+    }),
+    new webpack.DefinePlugin({
+      API_URL: JSON.stringify("http://api-for-project.com")
     })
-  ],
-  devServer: {
-    overlay: true
-  },
-  devtool: "#sourcemap"
+  ];
+
+  let optimazation = {};
+
+  if (isProdMode) {
+    plugins.push();
+    optimazation = Object.assign(optimazation, {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true
+        })
+      ]
+    });
+  }
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "index_bundle.js"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    devtool: isProdMode ? "none" : "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node-modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            "style-loader",
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: { sourceMap: true }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+                config: {
+                  path: "src/postcss.config.js"
+                }
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: { sourceMap: true }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: plugins,
+    optimization: optimazation,
+    devServer: {
+      overlay: true
+    }
+  };
 };
 
 // react-hot-loader
